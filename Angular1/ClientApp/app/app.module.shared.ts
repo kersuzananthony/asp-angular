39c86225b5fd7d5fc0e91561ae260370a55442fd
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-11-05 18:08:10 +0100

    Remove sample code from generator

--- Angular1/ClientApp/app/app.module.shared.ts
+++ Angular1/ClientApp/app/app.module.shared.ts
@@ -8,8 +8,6 @@ import {ToastyModule} from "ng2-toasty";
 import {AppComponent} from './components/app/app.component';
 import {NavMenuComponent} from './components/navmenu/navmenu.component';
 import {HomeComponent} from './components/home/home.component';
-import {FetchDataComponent} from './components/fetchdata/fetchdata.component';
-import {CounterComponent} from './components/counter/counter.component';
 import {VehicleFormComponent} from "./components/vehicle-form/vehicle-form.component";
 import {VehiclesService} from "./services/vehicles.service";
 import {AppErrorHandler} from "./app.error-handler";
@@ -19,8 +17,6 @@ import {VehicleListComponent} from "./components/vehicle-list/vehicle-list.compo
   declarations: [
     AppComponent,
     NavMenuComponent,
-    CounterComponent,
-    FetchDataComponent,
     HomeComponent,
     VehicleFormComponent,
     VehicleListComponent
@@ -33,8 +29,6 @@ import {VehicleListComponent} from "./components/vehicle-list/vehicle-list.compo
     RouterModule.forRoot([
       {path: '', redirectTo: 'vehicles', pathMatch: 'full'},
       {path: 'home', component: HomeComponent},
-      {path: 'counter', component: CounterComponent},
-      {path: 'fetch-data', component: FetchDataComponent},
       {path: 'vehicles', component: VehicleListComponent},
       {path: "vehicles/new", component: VehicleFormComponent},
       {path: "vehicles/:id", component: VehicleFormComponent},
